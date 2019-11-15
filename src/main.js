ead92ad592acab1bde179014899530e832d8ec91
Michael Wells <mwells1286@gmail.com>
2019-11-15 13:06:02 -0800

    add front end work to debug prevent default issue.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import { User } from './calc.js';
+
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready() {
+$(document).ready(function() {
   $("#user-input").submit(function(event) {
     event.preventDefault();
-    
-  })
-}
+    event.stopPropagation();
+    let userName = $('#user-name').val();
+    let userAge = $('#user-age').val();
+    let userDeathAge = $('#user-death-age').val();
+    let user = new User(userName, userAge, userDeathAge);
+$('#other-age').text(user.age);
+
+    // Object.keys(user.aging).forEach(key=>{
+    //   $('#aging-selector').append("<option value=" + `${user.aging[key]}` + ">" + `${key}` + "</option>");
+    // });
+  });
+});
