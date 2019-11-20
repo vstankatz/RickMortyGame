de9631170319d06566a61264ea48fb57f975ba9a
Student <example@example.com>
2019-11-20 11:57:22 -0800

    add images for character choice

--- src/apicalls.js
+++ src/apicalls.js
@@ -1,13 +1,33 @@
+
 export class Characters {
+  constructor(mainCharacter) {
+    this.mainCharacter = mainCharacter;
+    this.image = [];
+    this.name = [];
+  }
 
   async getCharacters(character) {
     try {
       let response = await fetch(`https://rickandmortyapi.com/api/character/${character}`)
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
-  } catch(error) {
+    } catch(error) {
 
-    console.error("There was an error handling your request: " + error.message);
+      console.error("There was an error handling your request: " + error.message);
+    }
   }
-}
+
+  async initialChars() {
+    try {
+      let charArray = [1,2,5];
+      for (let i=0; i<charArray.length; i++) {
+        let thisResponse = await this.getCharacters(charArray[i]);
+        this.image.push(thisResponse.image);
+        this.name.push(thisResponse.name);
+      }
+      console.log(this.name, this.image);
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -4,24 +4,36 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+const mainCharacter = new Characters();
+let charName = mainCharacter.name[1];
+function getElements(response) {
+  $(`img#${charName}`).attr('src', response.image);
+  console.log(response.image);
+}
+
+(async function characterGrab(character) {
+  const response = await mainCharacter.getCharacters(character);
+  getElements(response);
+  console.log("async function");
+})();
+
+(async function intialCharacter() {
+await mainCharacter.initialChars();
+console.log(mainCharacter.image[0]);
+$('img#rick').attr('src', mainCharacter.image[0]);
+$('img#morty').attr('src', mainCharacter.image[1]);
+$('img#jerry').attr('src', mainCharacter.image[2]);
+})();
+
 
 
 $(document).ready(function() {
 
-  const mainCharacter = new Characters();
 
 $('form#pickCharacter').submit(function(event) {
   event.preventDefault();
   let character = $("input[name=characterChoice]:checked").val();
   console.log(character);
-    (async() => {
-      const response = await mainCharacter.getCharacters(character);
-      getElements(response);
-      console.log("async function");
-    })();
-  function getElements(response) {
-    $(`img#morty`).attr('src', response.image);
-    console.log(response.image);
-  }
+
 })
 });
